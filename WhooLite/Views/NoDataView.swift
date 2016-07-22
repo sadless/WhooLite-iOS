076c290c69496dc46099029bd0345f1ac6cdea82
sadless <bhythmmaker@gmail.com>
2016-07-22 21:46:44 +0900

    commit

---
+++ WhooLite/Views/NoDataView.swift
@@ -0,0 +1,23 @@
+//
+//  NoDataView.swift
+//  WhooLite
+//
+//  Created by 안영건 on 2016. 7. 22..
+//  Copyright © 2016년 영건닷컴. All rights reserved.
+//
+
+import UIKit
+
+class NoDataView: UIView {
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var retryButton: UIButton!
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
